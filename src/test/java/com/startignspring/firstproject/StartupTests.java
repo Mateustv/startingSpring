9f8f2a43b30e72f9a0776c6c05190b747bc89475
Mateus Tavares <mateustavarestv@hotmail.com>
2023-07-24 22:55:55 -0300

    hello world

---
+++ src/test/java/com/startignspring/firstproject/StartupTests.java
@@ -0,0 +1,13 @@
+package com.startignspring.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
